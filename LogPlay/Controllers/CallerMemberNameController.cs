89f8de0620bbe560064bda678269de2d6ada2074
ashley <kilgourkilgour@hotmail.com>
2018-03-02 20:49:48 +0000

    Added CallerMemberNameController and its view

---
+++ LogPlay/Controllers/CallerMemberNameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LogPlay.Controllers
+{
+    public class CallerMemberNameController : Controller
+    {
+        // GET: CallerMemberName
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
