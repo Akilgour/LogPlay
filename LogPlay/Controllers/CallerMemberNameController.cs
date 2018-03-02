ea6497b86b283acd4eaf00190b84b3a25c472ada
ashley <kilgourkilgour@hotmail.com>
2018-03-02 21:20:14 +0000

    Added CallerMemberName

--- LogPlay/Controllers/CallerMemberNameController.cs
+++ LogPlay/Controllers/CallerMemberNameController.cs
@@ -11,7 +11,26 @@ namespace LogPlay.Controllers
         // GET: CallerMemberName
         public ActionResult Index()
         {
+            System.Diagnostics.Trace.WriteLine("Entering CallerMemberNameController.Index ");
+            LevelOne();
             return View();
+        }
+
+        private void LevelOne([System.Runtime.CompilerServices.CallerMemberName] string memberName = "")
+        {
+            System.Diagnostics.Trace.WriteLine("Entering CallerMemberNameController.LevelOne ");
+            System.Diagnostics.Trace.WriteLine($"memberName{memberName}");
+            LevelTwo("foo");
+            System.Diagnostics.Trace.WriteLine("Exiting CallerMemberNameController.LevelOne ");
+
+        }
+
+        private void LevelTwo(string value, [System.Runtime.CompilerServices.CallerMemberName] string memberName = "")
+        {
+            System.Diagnostics.Trace.WriteLine("Entering CallerMemberNameController.LevelTwo ");
+            System.Diagnostics.Trace.WriteLine($"value {value}");
+            System.Diagnostics.Trace.WriteLine($"memberName {memberName}");
+            System.Diagnostics.Trace.WriteLine("Exiting CallerMemberNameController.LevelTwo ");
         }
     }
 }
