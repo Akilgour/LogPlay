1a510096212899fd63458b4d569e4752a319e48f
ashley <kilgourkilgour@hotmail.com>
2018-03-03 21:46:32 +0000

    Stubbed XeretecLogging and its interface

---
+++ LogPlay/Logging/XeretecLogging.cs
@@ -0,0 +1,13 @@
+﻿using LogPlay.Logging.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogPlay.Logging
+{
+    public class XeretecLogging : IXeretecLogging
+    {
+
+    }
+}
