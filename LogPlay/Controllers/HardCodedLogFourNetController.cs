db714a04cd71b2ef7c2051a3c02c9473e5218a4c
ashley <kilgourkilgour@hotmail.com>
2018-03-03 19:57:08 +0000

    Added logging

--- LogPlay/Controllers/HardCodedLogFourNetController.cs
+++ LogPlay/Controllers/HardCodedLogFourNetController.cs
@@ -1,8 +1,4 @@
 ﻿using NLog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace LogPlay.Controllers
@@ -14,7 +10,7 @@ namespace LogPlay.Controllers
         {
             var config = new NLog.Config.LoggingConfiguration();
             var logfile = new NLog.Targets.FileTarget() { FileName = "file.txt", Name = "logfile" };
-            config.LoggingRules.Add(new NLog.Config.LoggingRule("*", LogLevel.Debug, logfile));
+            config.LoggingRules.Add(new NLog.Config.LoggingRule("*", LogLevel.Error, logfile));
             LogManager.Configuration = config;
             ILogger logger = LogManager.GetCurrentClassLogger();
 
@@ -24,7 +20,6 @@ namespace LogPlay.Controllers
             logger.Info("Normal behavior like mail sent, user updated profile etc.");
             logger.Debug("Executed queries, user authenticated, session expired");
             logger.Trace("Begin method X, end method X etc");
-
             return View();
         }
     }
