daf6e4f10c2a91cfbef3aa75b8838b08a935b77b
ashley <kilgourkilgour@hotmail.com>
2018-03-03 21:35:15 +0000

    Added Autofac config

--- LogPlay/Autofac/AutofacConfiguration.cs
+++ LogPlay/Autofac/AutofacConfiguration.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace LogPlay.Autofac
 {
-    public class AutofacConfiguration
+    public static class AutofacConfiguration
     {
+        public static void Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register MVC controllers.
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
     }
 }
