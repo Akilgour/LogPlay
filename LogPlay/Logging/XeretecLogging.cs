d7218294672ad9ba7e3979ad0d96fd8f42d16769
ashley <kilgourkilgour@hotmail.com>
2018-03-04 00:01:09 +0000

    added logging

--- LogPlay/Logging/XeretecLogging.cs
+++ LogPlay/Logging/XeretecLogging.cs
@@ -21,31 +21,37 @@ namespace LogPlay.Logging
         public void Debug(string loggingMessage, [CallerMemberName] string memberName = "")
         {
             System.Diagnostics.Trace.WriteLine($"Debug Message from {memberName}");
+            _logger.Debug(loggingMessage);
         }
 
         public void Error(string loggingMessage, [CallerMemberName] string memberName = "")
         {
             System.Diagnostics.Trace.WriteLine($"Error Message from {memberName}");
+            _logger.Error(loggingMessage);
         }
 
         public void Fatal(string loggingMessage, [CallerMemberName] string memberName = "")
         {
             System.Diagnostics.Trace.WriteLine($"Fatal Message from {memberName}");
+            _logger.Fatal(loggingMessage);
         }
 
         public void Info(string loggingMessage, [CallerMemberName] string memberName = "")
         {
             System.Diagnostics.Trace.WriteLine($"Info Message from {memberName}");
+            _logger.Info(loggingMessage);
         }
 
         public void Trace(string loggingMessage, [CallerMemberName] string memberName = "")
         {
             System.Diagnostics.Trace.WriteLine($"Trace Message from {memberName}");
+            _logger.Trace(loggingMessage);
         }
 
         public void Warn(string loggingMessage, [CallerMemberName] string memberName = "")
         {
             System.Diagnostics.Trace.WriteLine($"Warn Message from {memberName}");
+            _logger.Warn(loggingMessage);
         }
     }
 }
