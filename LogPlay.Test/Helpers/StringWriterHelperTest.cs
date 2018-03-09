f5d78c189b1c4288a45a3b4277f4460dc0c251eb
ashley <kilgourkilgour@hotmail.com>
2018-03-09 23:00:48 +0000

    Added StringWriterHelper and its Test

---
+++ LogPlay.Test/Helpers/StringWriterHelperTest.cs
@@ -0,0 +1,27 @@
+﻿using LogPlay.Helpers;
+using LogPlay.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogPlay.Test.Helpers
+{
+    [TestFixture]
+    public class StringWriterHelperTest
+    {
+
+        [Test]
+        public void Resolve()
+        {
+            //Arrange 
+            var person = new Person() { FirstName = "Ash", Surname = "Kilgour" };
+            //Act  
+            var value = StringWriterHelper.Resolve(person);
+            //Assert
+            Assert.AreEqual("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<Person xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <Id>0</Id>\r\n  <FirstName>Ash</FirstName>\r\n  <Surname>Kilgour</Surname>\r\n  <CreatedOn>0001-01-01T00:00:00</CreatedOn>\r\n  <Active>false</Active>\r\n</Person>", value);
+        }
+    }
+}
---
+++ LogPlay/Helpers/StringWriterHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace LogPlay.Helpers
+{
+    public static class StringWriterHelper
+    {
+        public static string Resolve(object obj)
+        {
+            XmlSerializer serializer = new XmlSerializer(obj.GetType());
+            using (StringWriter writer = new StringWriter())
+            {
+                serializer.Serialize(writer, obj);
+                return writer.ToString();
+            }
+        }
+    }
+}
