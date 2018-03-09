7b3c89be9252f2fea5e387cc585299b031e8173f
ashley <kilgourkilgour@hotmail.com>
2018-03-09 23:15:57 +0000

    Added a salesorder and salesorderitems

---
+++ LogPlay/Models/SalesOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogPlay.Models
+{
+    public class SalesOrder
+    {
+        public string OrderId { get; set; }
+        public List<SalesOrderItem> SalesOrderItems { get; set; }
+    }
+}
