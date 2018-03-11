f829d13f54e6902070be561adf25e1900832fda1
ashley <kilgourkilgour@hotmail.com>
2018-03-11 23:43:50 +0000

    Added StaticLogHelper and a button to call it

---
+++ LogPlay/Helpers/StaticLogHelper.cs
@@ -0,0 +1,24 @@
+﻿using LogPlay.Logging.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogPlay.Helpers
+{
+    public static class StaticLogHelper
+    {
+        public static string Resolve(IXeretecLogging logging, string orderId)
+        {
+            logging.Fatal("Highest level: important stuff down");
+            logging.Error("For example application crashes / exceptions.");
+            logging.Warn("Incorrect behavior but the application can continue");
+            logging.Info("Normal behavior like mail sent, user updated profile etc.");
+            logging.Debug("Executed queries, user authenticated, session expired");
+            logging.Trace("Begin method X, end method X etc");
+
+            return "StaticLogHelper Resolve";
+        }
+
+    }
+}
