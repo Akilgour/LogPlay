bb13c90081ea9cdc92f42172220306ee51dd616e
ashley <kilgourkilgour@hotmail.com>
2018-03-20 00:30:51 +0000

    removed my email

--- LogPlay/Helpers/SendEmailHelper.cs
+++ LogPlay/Helpers/SendEmailHelper.cs
@@ -17,7 +17,7 @@ namespace LogPlay.Helpers
                 var client = new SendGridClient(apiKey);
                 var from = new EmailAddress("test@example.com", "Example User");
                 var subject = "Sending with SendGrid is Fun 1";
-                var to = new EmailAddress("ashley.kilgour@googlemail.com", "Example User");
+                var to = new EmailAddress("test.test@googlemail.com", "Example User");
                 var plainTextContent = "and easy to do anywhere, even with C#";
                 var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
                 var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
