8334c7b3d825704f33b9dee16fdb42c0a6283cd5
ashley <kilgourkilgour@hotmail.com>
2018-03-20 00:09:36 +0000

    Added sendgrid Added test of send grid by giving it its own controller

---
+++ LogPlay/Controllers/SendGridController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace LogPlay.Controllers
+{
+    public class SendGridController : Controller
+    {
+        // GET: SendGrid
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendEmail()
+        {
+            Execute().Wait();
+            return RedirectToAction("Index");
+        }
+
+        static async Task Execute()
+        {
+
+            try
+            {
+                var apiKey = System.Configuration.ConfigurationManager.AppSettings["SENDGRID_API_KEY"];
+                var client = new SendGridClient(apiKey);
+                var from = new EmailAddress("test@example.com", "Example User");
+                var subject = "Sending with SendGrid is Fun";
+                var to = new EmailAddress("test@test.com", "Example User");
+                var plainTextContent = "and easy to do anywhere, even with C#";
+                var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var response = await client.SendEmailAsync(msg);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+
+    }
+}
